93d02ed55050e27deaa56f3e209c86d9b92bfa6f
Fasakin Henry <fasakinhenry@gmail.com>
2024-12-24 00:39:04 +0100

    feat: add code for main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+  // Remove all show and border classes
+  removeBorder();
+  removeShow();
+  // Add border to current tab item
+  this.classList.add('tab-border');
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add('show');
+}
+
+// Listen for tab click
+tabItems.forEach((item) => item.addEventListener('click', selectItem));
